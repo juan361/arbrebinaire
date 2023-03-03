08ca431f58a951746ca862a8614bf28e9d69bd4d
Juan <jgsan360@gmail.com>
2023-03-03 14:26:43 +0100

    ajout des fichiers .h

---
+++ members.h
@@ -0,0 +1,17 @@
+#ifndef MEMBERS_H
+#define MEMBERS_H
+enum MEMBER_STATUS{ACT,DCD,RET};
+static const int max_name_length=250;
+struct MEMBER{
+char* firstname;
+char* lastname;
+int birth_date;
+int promotion;
+int status;
+};
+typedef struct MEMBER member;
+member *newMEMBER();
+void deleteMember(member *M);
+member *initializeMember(member *m,char *fn,char *ln,int bd,int p,int st);
+member *createMember(char *fn,char *ln,int bd,int p,int st);
+#endif
---
+++ members_tools.h
@@ -0,0 +1,12 @@
+#ifndef MEMBERS_TOOLS_H
+#define MEMBERS_TOOLS_H
+#include"Members.h"
+#include<stdio.h>
+int Members_administration(char *filename);
+int scanfMember(member *M);
+int printfMember(member *M);
+int fscanfMember(FILE *file,member *M);
+int fprintfMember(FILE *file,member *M);
+int readMember(FILE *file,member *M);
+int writeMember(FILE *file,member *M);
+#endif
